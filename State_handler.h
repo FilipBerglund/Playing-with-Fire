6478b0caf86563e57e404e067520e391f63a1d53
Anders Henricsson <andhe641@student.liu.se>
2019-11-16 15:35:01 +0100

    Created the State_handler class

---
+++ State_handler.h
@@ -0,0 +1,38 @@
+#ifndef STATE_HANDLER
+#define STATE_HANDLER
+
+#include <string>
+#include <list>
+#include <SFML/Graphics.hpp>
+#include "State.h"
+
+class State_handler
+{
+public:
+	State_handler();
+	void update(sf::Mouse& mouse, sf::Keyboard& keyboard) 
+	{
+		current_state->update(mouse, keyboard);
+	}
+	void draw(sf::Window) {current_state->draw(sf::Window);}
+	void change_state();
+
+private:
+	State * current_state;
+	std::string current_state_string:
+	std::list<State> states;
+};
+
+void State_handler::change_state()
+{
+	if (!(current_state.name == current_state_string))
+		for (State &state: states)
+		{
+			if (state.name == current_state_string)
+			{
+				current_state = state;
+			}
+		}
+}
+
+#endif
