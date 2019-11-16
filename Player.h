eb06f18f46135885f874ffa58bcdb14c8dc915df
davma864 <davma864@su17-109.ad.liu.se>
2019-11-16 17:34:33 +0100

    Börjat skriva fler funktioner i Player.

--- Player.h
+++ Player.h
@@ -4,6 +4,8 @@
 #include <SFML/Graphics.hpp>
 #include "Game_object.h"
 #include <vector>
+#include <Bomb>
+#include <string>
 
 class Player: public Game_object
 {
@@ -17,11 +19,60 @@ public:
         bomb_cds.push_back(cd);
     }
 
+    Bomb* drop_bomb()  //Hjälpfunktion för när bomber ska droppas. 
+    {
+	for (int i = 0; i < bomb_cds.size(), i++)  //Går igenom hela listan av klockor. 
+	{
+	    if (bomb_cds[i].getElapsedTime() >= 5) //När detta uppfylls har spelaren möjligheten att droppa en bomb. 
+	    {
+		bomb_cds[i].restart();  
+		Bomb* ptr = new Bomb(this, fire_size);
+		return ptr;
+	    }
+	    else if (i == bomb_cds.size())  //När detta uppfylls har spelaren inte möjligheten att droppa en bomb.
+	    {
+		return nullptr;
+	    }
+	}
+    }
+
+    bool is_dead()  //Kollar om spelaren är död.
+    {
+	return health <= 0;
+    }
+
+    void new_round()  //Variabler återställs vid ny runda.
+    {
+	push_powerup = false;
+	int health = 3;
+	bonus_speed = 0;
+	fire_size = 0;
+	bomb_cds.resize(1);
+	bomb_cds[0].restart(); 
+    }
+
+    bool collision(Game_object* object) override
+    {
+        return object->get_hitbox().intersects(get_hitbox();
+    }
+
+    void apply(Game_object) override 
+
+    void give_points(int add_score)
+    {
+	score += add_score;
+    }
+
+    virtual void update(sf::Keyboard keyboard)
+
+
 private:
-    bool push_powerup;
-    uint health;
-    uint bonus_speed;
-    uint fire_size;
+    bool push_powerup; 
+    int health; 
+    int bonus_speed;
+    int fire_size;
+    int score;
+    Sprite sprite;
     std::vector<sf::Clock> bomb_cds;
     friend class Speed;
     friend class Extra_bomb;
