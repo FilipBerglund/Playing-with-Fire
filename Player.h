6cebafa1a2cf9f1d1b46e4dfac19bd7832658e50
davma864 <davma864@su12-106.ad.liu.se>
2019-11-20 14:52:20 +0100

    Alla medlemsvariabler har nu getters och setters. new_round har nu argument. Spritens origin ar i mitten.

--- Player.h
+++ Player.h
@@ -16,74 +16,49 @@ Saker som ej är klara eller otydliga
 2. Har funktionerna bool is_gliding(), void set_is_gliding(bool) 
    och void gliding(string) implementerats i Bomb?
 
-3. När en spelare blir skadad så blir spelaren immun most
+3. När en spelare blir skadad så blir spelaren immun mot
    skada i 5 sekunder. Är detta en lämplig siffra?
 
 4. Ska fire_size inledningsvis har värdet 2?
 
 5. Har Game_object en konstruktor av typen Game_object(pos, old_pos, sprite)
    som används nedanför? 
+
+6. Har Bomb en konstruktor av Bomb(Player* ptr, int fire_size)?
+
+7. Hur ska vi implementera update för NPC? 
 */
 
 class Player: public Game_object
 {
 public:
-    Player(sf::Vector2f pos, sf::Vector2f old_pos, sf::Sprite sprite):
+    Player(sf::Vector2f pos, sf::Vector2f old_pos, sf::Sprite sprite, int bombcd):
 	Game_object(pos, old_pos, sprite), push_powerup{false}, health{3},
-        bonus_speed{0}, fire_size{2}, score{0}, immune_clock{}, bomb_cds{}
+        bonus_speed{0}, fire_size{2}, score{0}, immune_clock{}, bomb_cds{}, bomb_cd{bombcd}
     {
-        bomb_cds.push_back();  
+        bomb_cds.push_back();  //Listan får storlek 1.
+	sprite.setOrigin(sprite->getTextureRect.width/2, sprite->getTextureRect.height/2) //Origin blir i mitten.
     }
 
-    Bomb* drop_bomb()  //Hjälpfunktion för när bomber ska droppas. 
-    {
-	for (int i = 0; i < bomb_cds.size(), i++)  //Går igenom hela listan av klockor. 
-	{
-	    if (bomb_cds[i].getElapsedTime() >= 5) //När detta uppfylls har spelaren möjligheten att droppa en bomb. 
-	    {
-		bomb_cds[i].restart();  
-		Bomb* ptr = new Bomb(this, fire_size);
-		return ptr;
-	    }
-	    else if (i == bomb_cds.size())  //När detta uppfylls har spelaren inte möjligheten att droppa en bomb.
-	    {
-		return nullptr;
-	    }
-	}
-    }
+    ~Player() = default;
 
-    bool is_dead()  //Kollar om spelaren är död.
+    void new_round(int initial_push, int initial_health, int initial_bonus_speed, int initial_fire_size)  //Variabler återställs vid ny runda.
     {
-	return health <= 0;
-    }
-
-    bool is_immune()
-    {
-	return immune_clock.getElapsedTime() <= 5;
-    }
-
-    void make_immune()
-    {
-	immune_clock.restart();
-    }
-
-    void new_round()  //Variabler återställs vid ny runda.
-    {
-	push_powerup = false;
-	int health = 3;
-	bonus_speed = 0;
-	fire_size = 0;
+	push_powerup = initial_push;
+	int health = initial_health;
+	bonus_speed = initial_bonus_speed;
+	fire_size = initial_fire_size;
 	bomb_cds.resize(1);
 	bomb_cds[0].restart(); 
     }
 
     bool collision(Game_object* object) override
     {
-        return object->get_hitbox().intersects(get_hitbox());
+        return object->hit_box().intersects(hit_box());
     }
 
     /*Spelarens applyfunktion ansvarar för vad som händer vid
-      kollision med ett annat object om är av typen Game_object.
+      kollision med ett annat object om det är av typen Game_object.
       Game_state tar själv bort powerups vid kollision med spelare
       så spelare behöver endast hantera kollision med bomber.*/ 
     void apply(Game_object* object) override
@@ -101,7 +76,7 @@ public:
 	      och sedan kollar vi spelarens and bombens ursprungliga positioner
 	      för att bestämma i vilken riktning bomben ska skjutas iväg.*/ 
             if (sprite.getPosition().x + sprite.getTextureRect().width/2 >=                
-		ptr->sprite.getPosition().x - ptr->sprite.getTextureRect().width/2 &&   
+		ptr->sprite.getPosition().x - ptr->sprite.getTextureRect().width / 2 &&   
 		old_position.x + sprite.getTextureRect().width/2 <
 		ptr->old_position.x - ptr->sprite.getTextureRect().width/2)
 	    {
@@ -133,17 +108,115 @@ public:
 	}
     }
 
-    void give_points(int add_score)
+    void set_push_powerup(bool var)
+    {
+	push_powerup = var;
+    }
+
+    bool get_push_powerup()
+    {
+	return push_powerup;
+    }
+
+    void reduce_health(int damage)
+    {
+	if (health - damage <= 0)
+	{
+	    health = 0;
+	}
+	else
+	{
+	    health -= damage;
+	}
+    }
+
+    int get_health()
+    {
+	return health;
+    }
+
+    bool is_dead()  //Kollar om spelaren är död.
+    {
+	return health == 0;
+    }
+
+    void increase_bonus_speed(int var)
+    {
+	bonus_speed += var;
+    }
+
+    int get_bonus_speed()
+    {
+	return bonus_speed;
+    }
+
+    void increase_fire_size(int var)
+    {
+	fire_size += var;
+    }
+
+    int get_fire_size()
+    {
+	return fire_size;
+    }
+
+    void increase_score(int add_score)
     {
 	score += add_score;
-    }                                                     
+    }
 
+    void get_score()
+    {
+	return score;
+    }
+
+    void increase_bombs()
+    {
+	bomb_cds.push_back(); 
+    }
+
+    bool is_immune()
+    {
+	return immune_clock.getElapsedTime() <= 5;
+    }
+
+    void make_immune()
+    {
+	immune_clock.restart();
+    }
+
+    Bomb* drop_bomb()  //Hjälpfunktion när bomber ska droppas. 
+    {
+	for (int i = 0; i < bomb_cds.size(), i++)  //Går igenom hela listan av klockor. 
+	{
+	    if (bomb_cds[i].getElapsedTime() >= bomb_cd) //När detta uppfylls har spelaren möjligheten att droppa en bomb. 
+	    {
+		bomb_cds[i].restart();  
+		Bomb* ptr = new Bomb(this, fire_size);
+		return ptr;
+	    }
+	    else if (i == bomb_cds.size())  //När detta uppfylls har spelaren inte möjligheten att droppa en bomb.
+	    {
+		return nullptr;
+	    }
+	}
+    }
+
+    const sf::IntRect & hit_box() override
+    {
+	return sprite.getTextureRect();
+    }
+
+    virtual void update(sf::Keyboard) = 0;
+
+    //update för NPC?
 private:
     bool push_powerup; 
     int health; 
     int bonus_speed;
     int fire_size;
     int score;
+    int bomb_cd;
     sf::Clock immune_clock;
     std::vector<sf::Clock> bomb_cds;
     
@@ -152,6 +225,5 @@ private:
     friend class Bigger_blast;
     friend class Push;
 };
-
 
 #endif
