59052a793125de2409c7e89cf3dec5f05a14e52d
Filip Berglund <filbe882@student.liu.se>
2019-11-20 13:33:08 +0100

    Adding pause and quit to Game_state

--- State.cc
+++ State.cc
@@ -16,6 +16,14 @@
 void Game_state::update(sf::Mouse mouse, sf::Keyboard keyboard)
 {
     user_input_handler(mouse, keyboard);
+
+    if (!is_playing)
+    {
+        //All clocks still run (both here and in player) so pausing will
+        //have unintended consequenses - exidental features.
+        return;
+    }
+
     for (Player& player : Players)
     {
         player.update(keyboard);
@@ -26,8 +34,10 @@ void Game_state::update(sf::Mouse mouse, sf::Keyboard keyboard)
         }
     }
 
+    check_collisions();
     wooden_boxes.remove_if(
             [](Wooden_box wooden_box){return wooden_box.is_dead()});
+
 
     if (is_round_over())
     {
@@ -139,8 +149,15 @@ void Game_state::user_input_handler(sf::Mouse mouse, sf::Keyboard keyboard)
 
     if (mouse.isButtonPressed(sf::Mouse::left)
     {
-        //end_game() and go to menu if quit_button.click(mouse)
-        //if pause_button.click(mouse) is_playing = !is_playing
+        if (quit_button.click(mouse))
+        {
+            current_state_string = "menu_state";
+            end_game();
+        }
+        if (pause_button.click(mouse))
+        {
+            is_playing = !is_playing
+        }
     }
 }
 
