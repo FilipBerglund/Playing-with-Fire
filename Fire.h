85cdf51528ad17ce4f3bcee2222fb2fd82258168
Linh Nguyen <linng299@student.liu.se>
2019-11-21 21:31:41 +0100

    Adding fire file

---
+++ Fire.h
@@ -0,0 +1,34 @@
+#ifndef FIRE_H
+#define FIRE_H
+
+#include <SFML/Graphics.hpp>
+
+#include "Box.h"
+#include "Game_state.h"
+#include "Player.h"
+
+
+class Fire: public Game_object
+{
+ public:
+    Fire (sf::Vector2f pos, sf::Sprite sprite):     
+    Game_objects(pos, sprite)
+    {}
+
+    fire();
+
+    bool is_boosted();
+    void fire_boost(int fire_size);
+
+    void give_points( Player owner);
+    void update() override;
+
+ private:
+    Player owner();
+    int timer();
+    int fire_size();
+    
+    sf::Clock fire_rate;
+
+};
+#endif        
