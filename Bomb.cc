39edcb52c2fefcb83b578a421959e4968f30040e
Linh Nguyen <linng299@student.liu.se>
2019-11-21 21:49:58 +0100

    Basic unfinished bomb

---
+++ Bomb.cc
@@ -0,0 +1,89 @@
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+#include "Fire.h"
+#include "Game_state"
+
+#include "Bomb.h"
+
+#include <iostream>
+using namespace std;
+
+Bomb::Bomb (sf::Vector2f pos, sf::Sprite sprite, Player* player):     
+    Game_objects(pos, sprite), owner{player}
+{}
+
+void Bomb::spawn_fire(Player owner, int timer, Fire fire)
+{
+    //adding owner condition
+    if (timer==0)
+    {
+        return fire;
+    }
+     
+}
+
+
+void Bomb::update()
+{
+
+}
+
+
+void Bomb::collision(Game_object* object)
+{
+    for (object == Box || object == Bomb || object == Player)
+    {
+       sprite.setPosition(old_postion);
+    }
+    
+}
+
+
+void Bomb::glide(string direction)
+{
+    Bomb* popo;
+      if (direction == "right")
+        {
+            /*bomb moves right till collision*/
+            
+        }
+        else if (direction == "left")
+        {
+            /* bomb moves left till collision */
+        }
+        else if (direction == "up")
+        {
+            /* bomb moves up till collision */
+        }
+        else if (direction == "down")
+        {
+            /* bomb moves down till collision */
+        }
+        else
+        {
+            popo->sprite.setposition(popo->old_position)
+        }
+        
+        
+        
+    
+    
+}
+
+bool Bomb::is_gliding() const
+{
+    if (is_gliding == false)
+    {
+        return glide;
+    }
+    else
+    {
+        old.position;
+    }
+    
+}
+
+void Bomb::set_is_gliding(bool sam)
+{
+    is_gliding = sam;
+}
