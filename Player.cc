01874a45fa057553910347bf7eac8cdfc91caece
Filip Berglund <filbe882@student.liu.se>
2019-11-21 18:02:57 +0100

    Fixing indentation and added includes to Player.cc

--- Player.cc
+++ Player.cc
@@ -1,12 +1,24 @@
-include "Player.h"
+#include "Player.h"
+#include "Bomb.h"
+#include <SFML/Graphics.hpp>
 
-
-Player::Player(sf::Vector2f pos, sf::Sprite sprite, int cooldown, int in_push,
-               int in_health, int in_speed, int in_fire, int in_cd):
-       Game_object(pos, sprite), push_powerup{in_push}, health{in_health}, speed{in_speed}, fire_size{in_fire},
-       score{0}, cd{in_cd}, immune_clock{}, bomb_cds{}, spawn_point{pos},
-       initial_push_powerup{in_push}, initial_health{in_health}, initial_speed{in_speed},
-       initial_fire_size{in_fire}, initial_cd{in_cd}
+Player::Player(sf::Vector2f pos, sf::Texture texture, int cooldown,
+        int in_push, int in_health, int in_speed, int in_fire, int in_cd):
+        Game_object(pos, texture),
+        push_powerup{in_push},
+        health{in_health},
+        speed{in_speed},
+        fire_size{in_fire},
+        score{0},
+        cd{in_cd},
+        immune_clock{},
+        bomb_cds{},
+        spawn_point{pos},
+        initial_push_powerup{in_push},
+        initial_health{in_health},
+        initial_speed{in_speed},
+        initial_fire_size{in_fire},
+        initial_cd{in_cd}
 {
     sf::Clock new_clock;
     bomb_cds.push_back(new_clock);  //Listan får storlek 1.
@@ -21,65 +33,65 @@ void Player::new_round()  //Variabler återställs vid ny runda.
     speed = initial_speed;
     fire_size = initial_fire_size;
     cd = initial_cd;
-    sprite.setPosition(spawn_point); 
+    sprite.setPosition(spawn_point);
     bomb_cds.resize(1);
-    bomb_cds[0].restart(); 
+    bomb_cds[0].restart();
 }
 
 
 /*Spelarens applyfunktion ansvarar för vad som händer vid
   kollision med ett annat object om det är av typen Game_object.
   Game_state tar själv bort powerups vid kollision med spelare
-  så spelare behöver endast hantera kollision med bomber.*/ 
-void Player::apply_on_hit_effect(Game_object* object) const 
+  så spelare behöver endast hantera kollision med bomber.*/
+void Player::apply_on_hit_effect(Game_object* object)
 {
     /*object kommer alltid vara en bombpekare men eftersom apply
       är överlagrad från Game_object så måste vi använda
       dyanmic_cast för att behandla object som en bombpekare.
       Därmed behöver vi inte kolla om pekaren ptr nedanför
-      är lika med nullptr.*/ 
+      är lika med nullptr.*/
     Bomb* ptr = dynamic_cast<Bomb*>(object);
-	
-    if (push_powerup == true)  
-    { 	    
-	/*I de stora if-satsern nedanför kollar vi först  kollision i ett visst led
-	  och sedan kollar vi spelarens and bombens ursprungliga positioner
-	  för att bestämma i vilken riktning bomben ska skjutas iväg.*/ 
-	if (sprite.getPosition().x + hitbox().width/2 >=                
-	    ptr->sprite.getPosition().x - ptr->hitbox().width/2 &&   
-	    old_position.x + hitbox().width/2 <
-	    ptr->old_position.x - hitbox().width/2)
-	{
-	    ptr->glide("right");
-	}
-	else if(sprite.getPosition().x - hitbox().width/2 <=
-		ptr->sprite.getPosition().x + ptr->hitbox().width/2 &&
-		old_position.x + hitbox().width/2 >
-		ptr->old_position.x - ptr->hitbox().width/2)
-	{
-	    ptr->glide("left");
-	}
-	else if(sprite.getPosition().y - hitbox().height/2 <=
-		ptr->sprite.getPosition().y + ptr->hitbox().height/2 &&
-		old_position.y + hitbox().height/2 >
-		ptr->old_position.y - ptr->hitbox().width/2)
-	{
-	    ptr->glide("up");
-	}
-	else  // If-sats ej nödvändig p.g.a. uteslutning. 
-	{
-	    ptr->glide("down");
-	}	   
+
+    if (push_powerup == true)
+    {
+        /*I de stora if-satsern nedanför kollar vi först  kollision i ett visst led
+          och sedan kollar vi spelarens and bombens ursprungliga positioner
+          för att bestämma i vilken riktning bomben ska skjutas iväg.*/
+        if (sprite.getPosition().x + hitbox().width/2 >=
+            ptr->sprite.getPosition().x - ptr->hitbox().width/2 &&
+            old_position.x + hitbox().width/2 <
+            ptr->old_position.x - hitbox().width/2)
+        {
+            ptr->glide("right");
+        }
+        else if(sprite.getPosition().x - hitbox().width/2 <=
+            ptr->sprite.getPosition().x + ptr->hitbox().width/2 &&
+            old_position.x + hitbox().width/2 >
+            ptr->old_position.x - ptr->hitbox().width/2)
+        {
+            ptr->glide("left");
+        }
+        else if(sprite.getPosition().y - hitbox().height/2 <=
+            ptr->sprite.getPosition().y + ptr->hitbox().height/2 &&
+            old_position.y + hitbox().height/2 >
+            ptr->old_position.y - ptr->hitbox().width/2)
+        {
+            ptr->glide("up");
+        }
+        else  // If-sats ej nödvändig p.g.a. uteslutning.
+        {
+            ptr->glide("down");
+        }
     }
     else if (ptr->is_gliding() == true)  //push_powerup == false så bomben ska stanna.
     {
-	ptr->sprite.setPosition(ptr->old_position);
-	ptr->set_is_gliding(false);
+        ptr->sprite.setPosition(ptr->old_position);
+        ptr->set_is_gliding(false);
     }
 }
 
 
-void Player::set_push_powerup(bool var) 
+void Player::set_push_powerup(bool var)
 {
     push_powerup = var;
 }
@@ -91,15 +103,15 @@ bool Player::get_push_powerup() const
 }
 
 
-void Player::reduce_health(int damage) 
+void Player::reduce_health(int damage)
 {
     if (health - damage <= 0)
     {
-	health = 0;
+        health = 0;
     }
     else
     {
-	health -= damage;
+        health -= damage;
     }
 }
 
@@ -153,7 +165,7 @@ int Player::get_score() const
 void Player::give_bomb()
 {
     sf::Clock new_clock;
-    bomb_cds.push_back(new_clock); 
+    bomb_cds.push_back(new_clock);
 }
 
 
@@ -169,16 +181,16 @@ void Player::make_immune()
 }
 
 
-Bomb* Player::drop_bomb()  //Hjälpfunktion när bomber ska droppas. 
+Bomb* Player::drop_bomb()  //Hjälpfunktion när bomber ska droppas.
 {
-    for (int i = 0; i < bomb_cds.size(), i++)  //Går igenom hela listan av klockor. 
+    for (int i = 0; i < bomb_cds.size(), i++)  //Går igenom hela listan av klockor.
     {
-	if (bomb_cds[i].getElapsedTime().asSeconds() >= cd) //När detta uppfylls har spelaren möjligheten att droppa en bomb. 
-	{
-	    bomb_cds[i].restart();  
-	    Bomb* ptr = new Bomb(this);
-	    return ptr;
-	}
+        if (bomb_cds[i].getElapsedTime().asSeconds() >= cd) //När detta uppfylls har spelaren möjligheten att droppa en bomb.
+        {
+            bomb_cds[i].restart();
+            Bomb* ptr = new Bomb(this);
+            return ptr;
+        }
     }
     return nullptr;  // Player kan inte droppa en bomb.
 }
--- Player.h
+++ Player.h
@@ -1,5 +1,5 @@
-#ifndef PLAYER
-#define PLAYER
+#ifndef PLAYER_H
+#define PLAYER_H
 
 #include <SFML/Graphics.hpp>
 #include "Bomb.h"
@@ -12,48 +12,48 @@ class Player: public Game_object
 {
 public:
     Player(sf::Vector2f pos, sf::Sprite sprite, int cooldown, int in_push,
-	   int in_health, int in_speed, int in_fire, int in_cd);
+        int in_health, int in_speed, int in_fire, int in_cd);
 
     ~Player() = default;
 
-    void new_round();  
-    void apply_on_hit_effect(Game_object* object) const override;
-    
+    void new_round();
+    void apply_on_hit_effect(Game_object* object) override;
+
     void set_push_powerup(bool var);
     bool get_push_powerup() const;
-	
+
     void reduce_health(int damage);
     int get_health() const;
-    bool is_dead() const;  
-	
+    bool is_dead() const;
+
     void increase_speed(int var);
     int get_speed() const;
-	
+
     void increase_fire_size(int var);
     int get_fire_size() const;
-	
+
     void increase_score(int add_score);
     int get_score() const;
-	
-    void give_bomb(); 
+
+    void give_bomb();
     bool is_immune() const;
     void make_immune();
     Bomb* drop_bomb();
 
 protected:
-    bool initial_push_powerup; 
+    bool initial_push_powerup;
     int initial_health;
     int initial_speed;
     int initial_fire_size;
     int initial_cd;
-    
-    bool push_powerup; 
-    int health; 
+
+    bool push_powerup;
+    int health;
     int speed;
     int fire_size;
     int score;
     int cd;
-    
+
     sf::Clock immune_clock;
     std::vector<sf::Clock> bomb_cds;
     sf::Vector2f spawn_point;
