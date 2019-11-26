80977f96764b495dfbf1c3ef19355336a168c6da
Dennis Abrikossov <denab905@student.liu.se>
2019-11-26 11:22:38 +0100

    Klar med otestad Int Button

--- Menu_button.cc
+++ Menu_button.cc
@@ -17,32 +17,7 @@ Menu_button::Menu_button(sf::Vector2f pos, sf::Sprite s):
   shape.setPosition(pos);
 }
 
-bool Menu_button::contains(sf::Mouse&)
-{
-  if(shape.getGlobalbounds)
-    {
-      return true;
-    }
-  else
-    {
-      return false;
-    }
-}
 
-int Menu_button::click(sf::Mouse&)
-{
-  // click returnera än int beroende på hur många npcs som skata med;
-  /*
-  if (spritezero.contatins(mouse.x, mouse.y))
-    {
-      return 0;
-    }
-  */
-  if()
-    {
-    }
-  
-}
 
 
 Int_button::Int_button(sf::Vector2f pos, sf::Texture s0, sf::Texture s1, sf::Texture s2, sf::Texture s3, sf::Texture s4 , sf::Texture sbg )
@@ -55,76 +30,71 @@ Int_button::Int_button(sf::Vector2f pos, sf::Texture s0, sf::Texture s1, sf::Tex
   
   //back grunds lådan
   //fixa corrdinter senare
-  sprite_backgroud.setPosition(pos);
-  sf::RectangleShape shape_sprite_background;
-  shape_sprite_background.setSize(sf::Vector2f(sprite_background.getsize()));
-  shape_sprite_background.setPosition(pos);
+  sprite_background.setPosition(pos);
+  //sf::RectangleShape shape_sprite_background;
+  //shape_sprite_background.setSize(sf::Vector2f(sprite_background.getSize()));
+  //shape_sprite_background.setPosition(pos);
 
   //sprite0 lådan
   sprite0.setPosition(sf::Vector2f(corrdx+50,corrdy+100));
-  sf::RectangleShape shape_0;
-  shape_sprite0.setSize(sf::Vector2f(sprite0.getsize());
-  shape_sprite0.setPosition(corrdx+50,corrdy+100);
+  //sf::RectangleShape shape_sprite0;
+  //shape_sprite0.setSize(sf::Vector2f(sprite0.getSize()));
+  //shape_sprite0.setPosition(corrdx+50,corrdy+100);
   
   //sprite1  lådan
   //fixa corrdinter senare 
   sprite1.setPosition(sf::Vector2f(corrdx+50,corrdy+100));
-  sf::RectangleShape shape_1;
-  shape_sprite1.setSize(sf::Vector2f(sprite1.getsize());
-  shape_sprite1.setPosition(corrdx+50,corrdy+100);
-
-  //sprite1  lådan
-  //fixa corrdinter senare 
-  sprite1.setPosition(sf::Vector2f(corrdx+50,corrdy+100));
-  sf::RectangleShape shape_1;
-  shape_sprite1.setSize(sf::Vector2f(sprite2.getsize());
-  shape_sprite1.setPosition(corrdx+50,corrdy+100);
+  //sf::RectangleShape shape_sprite1;
+  //shape_sprite1.setSize(sf::Vector2f(sprite2.getSize()));
+  //shape_sprite1.setPosition(corrdx+50,corrdy+100);
+  
   //sprite2  lådan
   //fixa corrdinter senare 
   sprite2.setPosition(sf::Vector2f(corrdx+50,corrdy+100));
-  sf::RectangleShape shape_2;
-  shape_sprite2.setSize(sf::Vector2f(sprite3.getsize());
-  shape_sprite2.setPosition(corrdx+50,corrdy+100);
+  //sf::RectangleShape shape_sprite2;
+  //shape_sprite2.setSize(sf::Vector2f(sprite3.getSize()));
+  //shape_sprite2.setPosition(corrdx+50,corrdy+100);
+  
   //sprite3  lådan
-  //fixa corrdinter senare 
-  sprite3.setPosition(sf::Vector2f(corrdx+50,corrdy+100));
-  sf::RectangleShape shape_3;
-  shape_sprite3.setSize(sf::Vector2f((sprite3.getsize());
-  shape_sprite3.setPosition(corrdx+50,corrdy+100);
+  //fixa corrdinter senare
+   sprite3.setPosition(sf::Vector2f(corrdx+50,corrdy+100));
+  //sf::RectangleShape shape_sprite3;
+  // shape_sprite3.setSize(sf::Vector2f((sprite3.getSize()));
+  // shape_sprite3.setPosition(corrdx+50,corrdy+100);
 
-  //sprite4  lådan
-  //fixa corrdinter senare 
+  //sprite4  lådan 
   sprite4.setPosition(sf::Vector2f(corrdx+50,corrdy+100));
-  sf::RectangleShape shape_4;
-  shape_sprite4.setSize(sf::Vector2f(sprite4.getsize());
-  shape_sprite4.setPosition(corrdx+50,corrdy+100);
+  //sf::RectangleShape shape_sprite4;
+  //shape_sprite4.setSize(sf::Vector2f(sprite4.getSize()));
+  // shape_sprite4.setPosition(corrdx+50,corrdy+100);
   
   
  }
 
-bool Int_button::click(sf::Mouse&)
+				     
+bool Int_button::click(sf::Mouse& mouse)
 {
-  if (sprite0.contains(mouse.x, mouse.y))
+  if (sprite0.getGlobalBounds().contains(mouse.getPosition().x , mouse.getPosition().y))
     {
       value = 0;
       return true;
     }
-  else if(sprite1.contains(mouse.x, mouse.y)
+  else if(sprite1.getGlobalBounds().contains(mouse.getPosition().x , mouse.getPosition().y))
     {
       value = 1;
       return true;
     }
-    else if(sprite2.contains(mouse.x, mouse.y)
+  else if(sprite2.getGlobalBounds().contains(mouse.getPosition().x , mouse.getPosition().y))
       {
 	value = 2;
 	return true;
       }
-      else if(sprite3.contains(mouse.x, mouse.y)
+  else if(sprite3.getGlobalBounds().contains(mouse.getPosition().x, mouse.getPosition().y))
 	{
 	  value = 3;
 	  return true;
 	}
-	else if(sprite4.contains(mouse.x, mouse.y)
+  else if(sprite4.getGlobalBounds().contains(mouse.getPosition().x, mouse.getPosition().y))
 	  {
 	    value = 4;
 	    return true;
@@ -137,5 +107,6 @@ bool Int_button::click(sf::Mouse&)
 
 }
 
+	
 
 
--- Menu_button.h
+++ Menu_button.h
@@ -12,8 +12,8 @@ public:
    Menu_button(sf::Vector2f pos, sf::Sprite s);
 
   
-  bool contains(sf::Mouse&);
-  bool click(sf::Mouse&);
+   // bool contains(sf::Mouse&);
+ virtual bool click(sf::Mouse&)=0;
 
   sf::Sprite get_drawable() const
     {
@@ -22,40 +22,45 @@ public:
   
  private:
   sf::Sprite sprite;
-  sf::Texture; 
+
 
 };
-/*
-class bool_button : public Menu_button
+
+class Bool_button : public Menu_button
 {
  Public:
- 
- private:
   
+    _button();
+   Menu_button(sf::Vector2f pos, sf::Sprite s);
+   
+ private:
+   sf::Sprite sprite;
   
 }
-*/
-class Int_button : public Menu_button
+
+class Int_button: public Menu_button
 {
- Public:
+ public:
   Int_button();
-  Int_button(sf::Vector2f pos, sf::Sprite s0, sf::Sprite s1, sf::Sprite s2, sf::Sprite s3, sf::Sprite s4 , sf::Sprite sbg);
+  Int_button(sf::Vector2f pos, sf::Texture s0, sf::Texture s1, sf::Texture s2, sf::Texture s3, sf::Texture s4 , sf::Texture sbg);
   
-   bool contains(sf::Mouse&);
-   bool click(sf::Mouse&);
+  //bool contains(sf::Mouse&);
+   bool click(sf::Mouse&) override;
    int get_value(){return value;}
    
  private:
    
    int value{};
-  sf::Texture sprite_background;
-  sf::Texture sprite0;
-  sf::Texture sprite1;
-  sf::Texture sprite2;
-  sf::Texture sprite3;
-  sf::Texture sprite4;
+  sf::Sprite sprite_background;
+  sf::Sprite sprite0;
+  sf::Sprite sprite1;
+  sf::Sprite sprite2;
+  sf::Sprite sprite3;
+  sf::Sprite sprite4;
+
+
   
-}
+};
 
 
 
