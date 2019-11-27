b85280234ec7fee4b2e16ae272217a09c2fa97dc
Linh Nguyen <linng299@student.liu.se>
2019-11-27 14:17:41 +0100

    Updated bomb.h with slight change of variables.

--- Bomb.h
+++ Bomb.h
@@ -11,20 +11,21 @@
 class Bomb: public Game_object
 {
  public:
-    Bomb (sf::Vector2f pos, sf::Sprite sprite, Player*);
-    void spawn_fire(Player* owner, int timer, Fire fire);
+    Bomb (sf::Vector2f pos, sf::Texture texture,
+     Player* player);
+    void spawn_fire(Player* owner, float timer, Fire fire);
     void update() override;
 
-    void collision (Game_object* object);
+    //void collision (Game_object*);
+    void apply_on_hit_effect(Game_object*);
 
     void glide(std::string direction);
     bool is_gliding() const;
     void set_is_gliding(bool sam);
 
  private:
-    std::string direction();
-    Player* owner();
-    int timer();
+    Player* owner;
+    float timer;
 
     sf::Clock ticking_bomb; 
 
