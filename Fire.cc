c803b47505450bc79269f225ecb0a3c35c9e0a42
linng299 <linng299@liu.student.se>
2019-12-01 22:40:31 +0100

    Fixed fire. Added hit_effect and boolean (check if fire is "alive") functions. Remove certain functions.

--- Fire.cc
+++ Fire.cc
@@ -1,54 +1,34 @@
 #include <SFML/Graphics.hpp>
 #include "Player.h"
-#include "Game_state"
 #include "Fire.h"
 
 Fire::Fire(sf::Vector2f pos, sf::Texture& sprite, Player* player) 
 : Game_objects(pos, sprite), owner{player}
 {
-  //init timer
-  std::initializer
+  aflame_timer.getElapsedTime().asSeconds()<=3;
 }
 
-<<<<<<< HEAD
-bool is_dead() const
-=======
-//TODO: Denna behövs inte.
-bool Fire::is_boosted()
+void apply_on_hit_effect(Game_object* object) override
 {
-    if (/*player boosted is applied*/)
+  dynamic_cast<Player*>(object)->reduce_health(1);
+  dynamic_cast<Wooden_box*>(object)->is_dead()==true;
+}
+
+bool Fire:: is_extinguished() const
+{
+  if(aflame_timer.getElapsedTime().asSeconds()>3)
     {
-        return fire_boost;
+      return true;
     }
-    else
-    {
-        /* code */
-    }
-    
-
-}
-//TODO: Denna behövs inte.
-void Fire::fire_boost(int fire_size)
->>>>>>> 93411fa2be2e3b8d5e9a5a3861169f1b421033ab
-{
-  return;
-}
-<<<<<<< HEAD
-
-void Fire::give_points(Player* owner)
-=======
-//TODO: Denna behövs inte.
-void Fire::give_points(Player owner)
->>>>>>> 93411fa2be2e3b8d5e9a5a3861169f1b421033ab
-{
-      return owner->increase_point(1);
+  
+  return false;
 }
 
-void update() override
+void Fire::update() override
 {
-<<<<<<< HEAD
   //Uppdaterar eldens tillstånd
-=======
-
->>>>>>> 93411fa2be2e3b8d5e9a5a3861169f1b421033ab
+    if(is_extinguished()==true)
+      {
+	//fire.remove
+      }
 }
--- Fire.h
+++ Fire.h
@@ -3,50 +3,22 @@
 
 #include <SFML/Graphics.hpp>
 #include "Box.h"
-#include "Game_state.h"
 #include "Player.h"
 
 
 class Fire: public Game_object
 {
  public:
-<<<<<<< HEAD
+  
     Fire (sf::Vector2f, sf::Texture, Player*);
-    bool is_dead() const;
-    void give_points(Player* owner);
-=======
-     //TODO: fire_size och timer behövs inte.
-    Fire (sf::Vector2f pos, sf::Sprite sprite, int timer, int fire_size, Player*);
-
-    //TODO: Dessa två funktioner behövs inte.
-    bool is_boosted();
-    void fire_boost(int fire_size);
-
-
-    //TODO: Lägg till funktionen apply_on_hit_effect.
-    //TODO: Denna funktion behövs inte.
-    void give_points( Player owner);
->>>>>>> 93411fa2be2e3b8d5e9a5a3861169f1b421033ab
+    
+    void apply_on_hit_effect(Game_object* object) override;
+    
     void update() override;
 
-    //TODO: lägg till en boolsk funktion som kollar om Fire
-    //ska tas bort från spelet.
-
+    bool is_extinguished() const;
+    
  private:
-<<<<<<< HEAD
-    Player* owner;
-    int timer;
-    int fire_size;
-=======
-    // TODO: Dessa tre är inte funktioner så skriv inte ().
-    // fire_size behövs inte.
-    Player owner();
-    int timer();
-    int fire_size();
->>>>>>> 93411fa2be2e3b8d5e9a5a3861169f1b421033ab
-
-    // TODO: Ha gärna ett tydilgare variabelnamn på denna.
-    sf::Clock fire_rate;
-
+    sf::Clock aflame_timer;
 };
 #endif        
