413e17c71a7f790920828501761b3b358529f4e8
anders <andhe641@student.liu.se>
2019-12-06 16:35:16 +0100

    Wrote the idea for the spawn_fire function

--- Bomb.cc
+++ Bomb.cc
@@ -5,3 +5,19 @@
 Bomb::Bomb(sf::Vector2f pos, sf::Texture texture, Player* player):
     Game_object(pos, texture), owner{player}
 {}
+
+void Bomb::spawn_fire(std::list<Wooden_box*> wooden_boxes,
+	       	std::list<Solid_box*> solid_boxes,
+	       	std::list<Fire*> fires)
+{
+	fires.push_back(new Fire fire1{});
+	for (i=0, i <= radius && solidbox_at_pos(pos.x+i),i++)
+	{
+		fires.push_back(new Fire fire{pox.x+i,pos.y, fire_texture}); // Need a sf::Vector2f here, also needs acces to fire texture...
+		if woodenbox_at_pos(pos.x+i,pos.y)
+		{
+			break;
+		}
+
+	}
+}
--- Bomb.h
+++ Bomb.h
@@ -13,7 +13,12 @@ public:
     void apply_on_hit_effect(Game_object*) {}
     void glide(std::string) {};
     bool is_gliding() {return false;}
+    void spawn_fire(std::list<Wooden_box*> wooden_boxes, 
+		    std::list<Solid_box*> solid_boxes,
+		    std::list<Fire*> fires);
 private:
     Player* owner;
+    bool solidbox_at_pos(x,y);
+    bool woodenbox_at_pos(x,y);
 };
 #endif
