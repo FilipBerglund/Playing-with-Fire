22ff234bc9f08876434f557fbc63796197198232
Anders Henricsson <andhe641@student.liu.se>
2019-12-10 11:17:36 +0100

    Wrote spawn_fire funciton

--- Bomb.cc
+++ Bomb.cc
@@ -8,16 +8,42 @@ Bomb::Bomb(sf::Vector2f pos, sf::Texture texture, Player* player):
 
 void Bomb::spawn_fire(std::list<Wooden_box*> wooden_boxes,
 	       	std::list<Solid_box*> solid_boxes,
-	       	std::list<Fire*> fires)
+	       	std::list<Fire*> fires,
+            sf::Texture fire_texture)
 {
+    sf::Vector2f pos{get_position()};
 	fires.push_back(new Fire fire1{});
-	for (i=0, i <= radius && solidbox_at_pos(pos.x+i),i++)
+	for (int i=0; i <= radius && solidbox_at_pos(pos.x+i, pos.y);i++)
 	{
-		fires.push_back(new Fire fire{pox.x+i,pos.y, fire_texture}); // Need a sf::Vector2f here, also needs acces to fire texture...
-		if woodenbox_at_pos(pos.x+i,pos.y)
+        sf::Vector2f newPos{pos.x + i, pos.y};
+		fires.push_back(new Fire fire{newPos, fire_texture});
+		if (woodenbox_at_pos(pos.x+i,pos.y))
 		{
 			break;
 		}
-
+    }
+	for (int i=0; i <= radius && solidbox_at_pos(pos.x-i,pos.y);i++)
+	{
+		fires.push_back(new Fire fire{pox.x-i,pos.y, fire_texture}); 
+		if (woodenbox_at_pos(pos.x-i,pos.y))
+		{
+			break;
+		}
 	}
+	for (int i=0; i <= radius && solidbox_at_pos(pos.x,pos.y+i);i++)
+	{
+		fires.push_back(new Fire fire{pox.x,pos.y+i, fire_texture}); 
+		if (woodenbox_at_pos(pos.x,pos.y+i))
+		{
+			break;
+		}
+    }
+	for (int i=0; i <= radius && solidbox_at_pos(pos.x,pos.y-i);i++)
+	{
+		fires.push_back(new Fire fire{pox.x,pos.y-i, fire_texture}); 
+		if (woodenbox_at_pos(pos.x,pos.y-i))
+		{
+			break;
+		}
+    }
 }
--- Bomb.h
+++ Bomb.h
@@ -4,6 +4,9 @@
 #include <SFML/Graphics.hpp>
 #include "Game_object.h"
 #include <string>
+#include <list>
+#include "Fire.h"
+#include "Box.h"
 
 class Player;
 class Bomb: public Game_object
@@ -15,10 +18,12 @@ public:
     bool is_gliding() {return false;}
     void spawn_fire(std::list<Wooden_box*> wooden_boxes, 
 		    std::list<Solid_box*> solid_boxes,
-		    std::list<Fire*> fires);
+		    std::list<Fire*> fires,
+            sf::Texture fire_texture);
 private:
     Player* owner;
-    bool solidbox_at_pos(x,y);
-    bool woodenbox_at_pos(x,y);
+    bool solidbox_at_pos(float x, float y);
+    bool woodenbox_at_pos(float x, float y);
+    int radius
 };
 #endif
