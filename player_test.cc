261e8fcf8daff63bc6630e90f665d85352ba7a73
Anders Henricsson <andhe641@student.liu.se>
2019-12-16 09:58:01 +0100

    Started writing tests from players

---
+++ player_test.cc
@@ -0,0 +1,33 @@
+#include "catch.hpp"
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+
+TEST_CASE()
+{
+    sf::Vector2f pos{50,100};
+    sf::Texture texture{};
+    texture.loadFromFile("textures/player1_texture.png");
+    Player player1{pos, texture, false, 3, 1, 2, 3, "player 1"};
+
+    CHECK_FALSE(player1.is_dead());
+    player1.reduce_health(3);
+    CHECK(player1.is_dead());
+
+    Player player2{pos, texture, false, 3, 1, 2, 3, "player 1"};
+    CHECK_FALSE(player2.is_immune());
+    Player * player1ptr{&player1};
+    Player * player2ptr{&player2};
+    /*
+    Fire fire{pos, texture, player1ptr};
+    player2.reduce_health(1);
+    CHECK_FALSE(player2.is_immune());
+    fire.apply_on_hit_effect(player2ptr); //Player health should reduce
+    CHECK(player2.get_health()==1); 
+    player2.make_immune();
+    fire.apply_on_hit_effect(player2ptr);
+    CHECK(player2.get_health()==1); // Player should be immune
+    */
+
+    CHECK(player2.request_to_drop_bomb());
+
+}
