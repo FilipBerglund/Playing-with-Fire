c6b942c8ed1dd5df4a292bb9930eb50ea012ed8f
Filip Berglund <filbe882@student.liu.se>
2019-12-17 14:28:23 +0100

    Reverted some changes in State_handler.h that caused mysterious segfault

--- State_handler.h
+++ State_handler.h
@@ -1,7 +1,6 @@
 #ifndef STATE_HANDLER_H
 #define STATE_HANDLER_H
 
-#include <string>
 #include <list>
 #include <SFML/Graphics.hpp>
 #include "State.h"
@@ -10,26 +9,24 @@ class State_handler
 {
 public:
     State_handler():
-        menu_state{new Menu_state},
-        game_state{new Game_state},
-        end_screen{new End_screen}
+    game_state{new Game_state}, menu_state{new Menu_state}, end_screen{new End_screen}
     {
-        *current_state = menu_state;
+        *current_state= menu_state;
     }
-    State_handler(const State_handler&) = delete;
-    State_handler operator=(const State_handler&) = delete;
-
     void update(sf::Mouse& mouse, sf::Keyboard& keyboard, sf::RenderWindow& window)
     {
-      (*current_state)->update(mouse, keyboard, game_state,
-                               menu_state, end_screen, current_state, window);
+      (*current_state)->update(mouse, keyboard, game_state, menu_state,
+              end_screen, current_state, window);
     }
     void draw(sf::RenderWindow& window) {(*current_state)->draw(window);}
 
 private:
+    State** current_state;
     Menu_state* menu_state;
     Game_state* game_state;
     End_screen* end_screen;
-    State** current_state;
+
+    //We get segfault without this. Any list is okay.
+    std::list<int> what_is_this_help;
 };
 #endif
