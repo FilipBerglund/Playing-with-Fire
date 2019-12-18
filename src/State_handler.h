43ef341d6e772bea1ef4f04316dbe409653c715b
Filip Berglund <filbe882@student.liu.se>
2019-12-18 14:01:46 +0100

    Moved functionality from Main.cc to State_handler. Now fps and window size are loaded from file

--- src/State_handler.h
+++ src/State_handler.h
@@ -7,24 +7,28 @@
 class State_handler
 {
 public:
-    State_handler():
-        menu_state{new Menu_state},
-        game_state{new Game_state},
-        end_screen{new End_screen},
-        current_state{menu_state}
-    {}
+    State_handler();
+    State_handler(const State_handler&) = delete;
+    State_handler& operator=(const State_handler&) = delete;
 
-    void update(sf::Mouse& mouse, sf::Keyboard& keyboard, sf::RenderWindow& window)
-    {
-      (current_state)->update(mouse, keyboard, game_state, menu_state,
-              end_screen, &current_state, window);
-    }
-    void draw(sf::RenderWindow& window) {(current_state)->draw(window);}
+    void update();
+    void draw();
+    void load_window_settings();
+    void run();
 
 private:
     Menu_state* menu_state;
     Game_state* game_state;
     End_screen* end_screen;
     State* current_state;
+    uint width;
+    uint height;
+    sf::RenderWindow window;
+    sf::Mouse mouse;
+    sf::Keyboard keyboard;
+    sf::Clock fps_clock;
+    double fps;
+    sf::Time target;
+    sf::Event event;
 };
 #endif
